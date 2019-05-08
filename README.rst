ead1e0deb6cdcdf28a1d9239a69856534c9fef91
sigvaldm <sigvaldm@fys.uio.no>
2019-05-08 16:17:47 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,23 @@
+localreg
+=========
+
+.. image:: https://travis-ci.com/sigvaldm/localreg.svg?branch=master
+    :target: https://travis-ci.com/sigvaldm/localreg
+
+.. image:: https://coveralls.io/repos/github/sigvaldm/localreg/badge.svg?branch=master
+    :target: https://coveralls.io/github/sigvaldm/localreg?branch=master
+
+.. image:: https://img.shields.io/pypi/pyversions/localreg.svg
+    :target: https://pypi.org/project/localreg
+
+Local Polynomial Regression
+
+Installation
+------------
+Install from PyPI using ``pip`` (preferred method)::
+
+    pip install localreg
+
+Or download the GitHub repository https://github.com/sigvaldm/localreg.git and run::
+
+    python setup.py install
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Copyright 2019 Sigvald Marholm <marholm@marebakken.com>
+
+This file is part of localreg.
+
+localreg is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+localreg is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with localreg  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup
+from io import open # Necessary for Python 2.7
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('.version') as f:
+    version = f.read().strip()
+
+setup(name='localreg',
+      version=version,
+      description='Local Polynomial Regression',
+      long_description=long_description,
+      author='Sigvald Marholm',
+      author_email='marholm@marebakken.com',
+      url='https://github.com/sigvaldm/localreg.git',
+      py_modules=['localreg'],
+      #install_requires=['scipy','frmt'],
+      license='LGPL',
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+        ]
+     )
+
