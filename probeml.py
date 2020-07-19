95083b7cf539d6eb0e444f31fa7ab6146e7f42e6
sigvaldm <sigvaldm@fys.uio.no>
2020-07-19 11:24:32 +0200

    Added documentation

--- probeml.py
+++ probeml.py
@@ -67,9 +67,42 @@ def gaussian(t):
 
 class RBFnet(object):
 
-    def train(self, input, output, centers, rbf, radius=1):
+    def train(self, input, output, centers=10, rbf=gaussian, radius=1):
+        """
+        Train the RBF net.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            input[i,j] is data point i, independent variable j
+
+        output: numpy.ndarray
+            output[i] is data point i, dependent variable
+
+        centers: int or numpy.ndarray
+            If centers is an array, the centers of the radial basis functions
+            are pre-specified, and centers[i,j] is center i, coordinate j. If
+            centers is an integer, it is the number of radial basis functions,
+            and their centers will be created internally by K-means clustering.
+
+        rbf: function
+            Which radial basis function to use. The function should take a
+            signel scalar argument, and is shifted according to the centers and
+            scaled according to the radius internally according to the
+            following equation:
+
+                rbf((input-center)/radius)
+
+        radius: float
+            The radius to use in rbf
+        """
+
+        if not isinstance(centers, np.ndarray):
+            centers = kmeans_centers(input, centers)
+
         assert input.shape[0]==output.shape[0]
         assert input.shape[1]==centers.shape[1]
+
         matrix = np.zeros((len(input), len(centers)), dtype=float)
         for i in tqdm(range(len(input))):
             for j in range(len(centers)):
@@ -80,21 +113,48 @@ class RBFnet(object):
         self.residual = residual
         self.rbf = rbf
         self.radius = radius
+        self.centers = centers
         # print(residual)
 
     def predict(self, input):
-        # print(input)
+        """
+        Make one or more predictions using the RBF net.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            input[i,j] is prediction point i, independent variable j
+
+        Returns
+        -------
+        numpy.ndarray where output[i] is the prediction of point i
+        """
         output = np.zeros(input.shape[0])
-        for j in range(len(centers)):
-            output += self.coeffs[j]*self.rbf(np.linalg.norm(input-centers[j,:], axis=1)/self.radius)
+        for j in range(len(self.centers)):
+            distance = np.linalg.norm(input-self.centers[j,:], axis=1)
+            output += self.coeffs[j]*self.rbf(distance/self.radius)
         return output
 
-    def validate(self, input, output):
+    def error(self, input, output):
+        """
+        The prediction error of the RBF net for a given set of data points.
+        This will be the training error, validation error or test error
+        depending on whether the set is the training set, the validation set
+        or the test set.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            input[i,j] is data point i, independent variable j
+
+        output: numpy.ndarray
+            output[i] is data point i, dependent variable
+        """
         predicted = self.predict(input)
         deviations = np.abs(predicted-output)
         reldev = deviations/output
         # print(np.linalg.norm(deviations)**2)
-        print_table(zip(output, predicted, deviations, reldev), format='{:g}')
+        # print_table(zip(output, predicted, deviations, reldev), format='{:g}')
         print(np.mean(reldev))
         # print_table(zip(input, output, predicted))
         # print(np.array(list(zip(input, output, predicted))))
@@ -106,13 +166,12 @@ data = read_RBF_data(sys.argv[1])
 currents = data[:,0:4]*1e6 # [uA]
 # density = data[:,5]*1e-12
 density = data[:,4]*1e-10
-centers = kmeans_centers(currents[:M,0:4], N)
+# centers = kmeans_centers(currents[:M], N)
+# print(currents.shape)
 # plot_data(currents, centers)
 
-# coeffs, residual = train(currents, centers, density, gaussian)
-# pred = predict(coeffs, currents[0,:], centers, gaussian)
 net = RBFnet()
-net.train(currents[:M], density[:M], centers, gaussian)
+net.train(currents[:M], density[:M], 100)
 pred = net.predict(currents[0:K,:])
 print(pred, density[0:K], (pred-density[0:K])/density[0:K])
-net.validate(currents[:M], density[:M])
+net.error(currents[:M], density[:M])
