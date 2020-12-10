81a706b2a32511b27d144a39c3993eb437a3da76
sigvaldm <sigvaldm@fys.uio.no>
2020-12-10 13:27:17 +0100

    Added testing for newer Python

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(name='localreg',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.9'
         ]
      )
 
