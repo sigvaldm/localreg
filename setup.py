a95a422821b8bc9483a642aa1b5a1d945f7353c8
sigvaldm <sigvaldm@fys.uio.no>
2020-12-14 13:56:52 +0100

    Bugfix in setup.py classifiers

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ setup(name='localreg',
       classifiers=[
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         ]
      )
 
